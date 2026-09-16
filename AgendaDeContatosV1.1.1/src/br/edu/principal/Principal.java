35dcf9b4d419f3bad877a888e0ca0c2745f98892
kalleocoelho05-png <kalleo.coelho05@aluno.ifce.edu.br>
2026-09-16 20:24:23 -0300

    Agenda de Contatos V1.1.1

---
+++ AgendaDeContatosV1.1.1/src/br/edu/principal/Principal.java
@@ -0,0 +1,38 @@
+package br.edu.principal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+
+        List<String> nomes = new ArrayList<>();
+        List<String> celulares = new ArrayList<>();
+        List<String> emails = new ArrayList<>();
+        int opcao;
+        boolean continuar = true;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        Uteis.mostraInicializacao(); 
+
+        while (continuar) {
+        	Uteis.mostraMenu();
+            opcao = Uteis.selecionaOpcao(sc);
+
+            switch (opcao) {
+                case 1-> Agenda.adicionar(sc, nomes, celulares, emails);           	
+                case 2-> Agenda.listar(nomes, celulares, emails);
+                case 3-> Agenda.pesquisar(sc, nomes, celulares, emails);
+                case 4-> Agenda.atualizar(sc, nomes, celulares, emails);           	
+                case 5-> Agenda.excluir(sc, nomes, celulares, emails);          	
+                case 6-> continuar = Uteis.sair();
+                case 7-> Uteis.sobre();
+                default -> System.out.println("Opção inválida!");
+            }
+        }
+        sc.close();
+    }
+}
